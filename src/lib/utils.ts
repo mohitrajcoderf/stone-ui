81c1335a441fc083a6d379be53af2e3d0b75a465
Mohit Raj <mohitraj12099@gmail.com>
2025-01-06 14:03:54 +0000

    update: config.tsx and utils.ts

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,11 @@
 import { clsx, type ClassValue } from "clsx"
 import { twMerge } from "tailwind-merge"
+import { siteConfig } from "./config";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
+}
+
+export function absoluteUrl(path: string) {
+  return `${process.env.NEXT_PUBLIC_APP_URL || siteConfig.url}${path}`;
 }
